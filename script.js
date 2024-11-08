12bf89153b288cfc74868eaa0d419f2e0591c9e1
ayush-singh-225 <ayushsinghtemp@gmail.com>
2024-11-08 15:34:06 +0530

    Assessment1 project

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementsByClassName('resume')[0].style.display = 'none';
+
+
+document.querySelector('.downpdf').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementsByClassName('resume-form')[0].style.display = 'none';
+    document.getElementsByClassName('resume')[0].style.display ='block';
+
+
+    const name1 = document.getElementById('name1').value;
+    const email = document.getElementById('email').value;
+    const phone = document.querySelector('.phone').value;
+    const linkedin = document.querySelector('.linkedin').value;
+    const github = document.querySelector('.github').value;
+    const experience = document.querySelector('.experience').value;
+    const education = document.querySelector('.education').value;
+
+    document.getElementById('name').textContent = name1;
+    document.getElementById('email').textContent = email;
+    document.getElementById('phone').textContent = phone;
+    document.getElementById('linkedin').textContent = linkedin;
+    document.getElementById('linkedin').href = linkedin;
+    document.getElementById('github').textContent = github;
+    document.getElementById('github').href = github;
+    document.getElementById('experience').textContent = experience;
+    document.getElementById('education').textContent = education;
+
+    const photoInput = document.querySelector('.photo');
+    if (photoInput.files && photoInput.files[0]) {
+        const photoURL = URL.createObjectURL(photoInput.files[0]);
+        document.getElementById('photo').src = photoURL;
+    }
+});
+
+function downloadResume(){
+    const resumeElement=document.getElementById('resume');
+
+    const options={
+        filename: 'Resume.pdf'
+    }
+
+    html2pdf().set(options).from(resumeElement).save();
+}
+
+
